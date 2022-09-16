3ed830db13a7f4a1d1d9f4776ad218707a6067c0
Alex <uvarovaas@ya.ru>
2022-09-16 23:12:58 +0700

    Добавлен ввод значений для суммирования

--- Ex003_Summ2/Program.cs
+++ Ex003_Summ2/Program.cs
@@ -1,3 +1,10 @@
-﻿int numberA = 1;
-int numberB = 59;
-Console.WriteLine(numberA + numberB);
+﻿Console.WriteLine("Введите первое целое число");
+string a = Console.ReadLine();
+int numberA = Convert.ToInt32(a);
+
+Console.WriteLine("Введите второе целое число");
+string b = Console.ReadLine();
+int numberB = Convert.ToInt32(b);
+
+int Summ = numberA + numberB;
+Console.WriteLine($"Сумма чисел равна {Summ}");
