b86783e7477f33d21868d941b120c3d2db7765de
Alex <uvarovaas@ya.ru>
2022-09-17 00:08:44 +0700

    добавлено особое приветствие пользователя

---
+++ Ex005_if-Else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользоваеля");
+string username = Console.ReadLine();
+
+if(username == "Cаша")
+{
+    Console.WriteLine("Ура, это же CАША");
+}
+else {
+    Console.WriteLine($"Привет, {username}");
+}
