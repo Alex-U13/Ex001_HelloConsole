afb402176e3f5d1ecd02607a42ec0ba1ea32e305
Alex <uvarovaas@ya.ru>
2022-09-16 23:50:20 +0700

    сложение случайных чисел

--- Ex003_Summ2/Program.cs
+++ Ex003_Summ2/Program.cs
@@ -1,10 +1,17 @@
-﻿Console.WriteLine("Введите первое целое число");
-string a = Console.ReadLine();
-int numberA = Convert.ToInt32(a);
+﻿//Console.WriteLine("Введите первое целое число");
+// string a = Console.ReadLine();
+//int numberA = Convert.ToInt32(a);
 
-Console.WriteLine("Введите второе целое число");
-string b = Console.ReadLine();
-int numberB = Convert.ToInt32(b);
+//Console.WriteLine("Введите второе целое число");
+//string b = Console.ReadLine();
+//int numberB = Convert.ToInt32(b);
 
-int Summ = numberA + numberB;
-Console.WriteLine($"Сумма чисел равна {Summ}");
+//int Summ = numberA + numberB;
+//Console.WriteLine($"Сумма чисел равна {Summ}"); 
+
+int numberA = new Random().Next(1, 10); // случайные целые цисла от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine($"Сумма двух случайных чисел {result}");
